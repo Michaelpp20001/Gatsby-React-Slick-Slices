f278b7fa864e2ea5da1f5c7dd9353b0e3b0a11a9
Michael <michaelpp2001@gmail.com>
2020-10-29 16:49:59 -0700

    added preview in sanity studio with pizza toppings references

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -40,5 +40,33 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: add custom input component
     },
+    // creating data relations
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    // destructuring, sticking everything we didnt destructure, into an object called toppings
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
