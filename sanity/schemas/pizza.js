1457a99a5e7ee0920f2eae2ca468a5f48261bd10
Michael <michaelpp2001@gmail.com>
2020-10-29 13:02:37 -0700

    added a pizza schema to sanity backend

---
+++ sanity/schemas/pizza.js
@@ -0,0 +1,44 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
+export default {
+  // computer name
+  name: 'pizza',
+  // visible title
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxlength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      // how to control your data, cant add a pizza less than $10
+      validation: (Rule) => Rule.min(1000),
+      // TODO: add custom input component
+    },
+  ],
+};
