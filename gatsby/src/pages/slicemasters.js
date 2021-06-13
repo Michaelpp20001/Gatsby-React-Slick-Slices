3d5ba86ea175d385128e82ed80c552c7dba7ad52
Michael <michaelpp2001@gmail.com>
2021-06-13 11:11:19 -0700

    added a query for slicemaster data, then console logged to verify

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,35 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters);
   return (
     <div>
       <p>Hey! I'm the slicemasters page</p>
     </div>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
