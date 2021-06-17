c1ab23d916943a1e8b4fe821ddb062772378cc2f
Michael <michaelpp2001@gmail.com>
2021-06-16 20:31:35 -0700

    added custom useForm hook in utils, added form in order page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import SEO from '../components/SEO';
+import useForm from '../utils/useForm';
 
 export default function OrderPage() {
+  const { values, updateValues } = useForm({
+    name: '',
+    email: '',
+  });
   return (
     <>
       <SEO title="Order a Pizza" />
-      <p>Hey! I'm the order page</p>
+      <form>
+        <fieldset>
+          <legend>Your Info</legend>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={updateValues}
+          />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+        </fieldset>
+        <fieldset>
+          <legend>Menu</legend>
+        </fieldset>
+        <fieldset>
+          <legend>Order</legend>
+        </fieldset>
+      </form>
     </>
   );
 }
